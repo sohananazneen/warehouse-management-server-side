7ba66af8afe74d6fcb46d7a1a3494b9e2aea69bc
SohanaN <sohananaz01@gmail.com>
2022-05-08 11:01:21 +0600

    Inventory api added

--- index.js
+++ index.js
@@ -26,12 +26,6 @@ async function run() {
             res.send(inventorys);
         });
 
-        app.get('/inventory/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const inventory = await inventoryCollection.findOne(query);
-            res.send(inventory);
-        });
 
     }
     finally {
