075ed5ec4190c8d56785158d52a285d93f7dc0d3
SohanaN <sohananaz01@gmail.com>
2022-05-09 03:56:33 +0600

    jwt implemented

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ app.use(cors());
 app.use(express.json());
 
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.322sa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -61,12 +77,18 @@ async function run() {
         });
 
         //My Items API
-        app.get('/restock', async (req, res) => {
+        app.get('/restock', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const email = req.query.email;
-            const query = { email: email };
-            const cursor = restockCollection.find(query);
-            const restocks = await cursor.toArray();
-            res.send(restocks);
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = restockCollection.find(query);
+                const restocks = await cursor.toArray();
+                res.send(restocks);
+            }
+            else {
+                res.status(403).send({ message: 'forbidden access' })
+            }
         })
 
         // Restock 
